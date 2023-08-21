c8e486b53c3005684193b50b4c699c8ad9548e48
Victor <63815582+VictorBaldez@users.noreply.github.com>
2023-08-21 10:10:43 -0300

    Update Area.java
    Replaced a 'for' loop with a 'do-while' loop to enhance code flow and ensure initial execution. The 'do-while' loop maintains the same logic while offering improved readability and adherence to the intended behavior.

--- OOP/Area.java
+++ OOP/Area.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[]args){
 		Scanner input = new Scanner(System.in);
-		System.out.println("Quantos triangulos deseja calcular a área?:");
-		int quantidade = input.nextInt();
-		for (int i=0; i < quantidade; i++) {
-			System.out.println("Insira o valor dos lados dos triângulos:");
+		System.out.println("Quantos triângulos deseja calcular a área?:");
+		String i;
+		do {
 			double x,y,z;
 			x = input.nextDouble();
 			y = input.nextDouble();
 			z = input.nextDouble();
 			double triangleArea = area(x,y,z);
-			
 			showResult(triangleArea);
-			}
+			System.out.println("Deseja realizar o cálculo da área de outro triângulo?:");
+			i = input.next();
+			} while ( i != "Sim");
 		input.close();
 	}
 	public static double area(double ladoA,double ladoB,double ladoC) {
@@ -29,6 +29,10 @@ public class Main {
 		return area;
 		
 	}
+	public static void showResult(double value) {
+		System.out.println("Área = " + value);
+	}
+}
 	public static void showResult(double value) {
 		System.out.println("Área = " + value);
 	}
