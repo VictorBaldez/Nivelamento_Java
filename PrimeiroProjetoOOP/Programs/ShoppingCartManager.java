3f8d86849fa6e0000dcd7fd013e43bc450711d1a
Victor <63815582+VictorBaldez@users.noreply.github.com>
2023-08-30 16:49:19 -0300

    Add files via upload

---
+++ PrimeiroProjetoOOP/Programs/ShoppingCartManager.java
@@ -0,0 +1,22 @@
+package org.example.sumary.PrimeiroProjetoOOP.Programs;
+import org.example.sumary.PrimeiroProjetoOOP.Entities.CarMethods;
+import org.example.sumary.PrimeiroProjetoOOP.Entities.CarOptions;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ShoppingCartManager {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+        CarOptions dealership = new CarOptions();
+        System.out.println("This is a Premium Automotive Dealership, what brand do are you looking for?.\n ");
+        dealership.BrandList();
+        CarMethods user = new CarMethods();
+        user.setCar_brand(input.nextLine());
+        dealership.brandModels(user.getCar_brand());
+        user.setModel(input.nextLine());
+        dealership.modelsInfo(user.getModel());
+
+    }
+}
